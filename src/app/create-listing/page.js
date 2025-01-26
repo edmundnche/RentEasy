d30123110f84a5252f583a8bcb96cb6e4d2943df
bishaltc <bishal.thapachhetri@edu.sait.ca>
2025-01-25 22:50:44 -0700

    listing page
    setting up for the listing page

---
+++ src/app/create-listing/page.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+function CreateListing() {
+  return (
+    <div>
+        
+    </div>
+  
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 export default function Home() {
   return (
-    <div className='text-red-500'>Home</div>
-  )
+    <>
+      <div className='text-red-500 text-xl font-semibold'>Home</div>
+      <Link href="/create-listing" className="text-blue-500 hover:underline">
+        Go to Create Listing Page
+      </Link>
+    </>
+  );
 }
