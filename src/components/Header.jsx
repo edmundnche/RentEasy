86e387cb82cf134a6e7073cffc7b175ad0c618b2
edmundnche <edmundche.nche@edu.sait.ca>
2025-01-25 21:46:49 -0700

    Add Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+import { FaSearchLocation } from "react-icons/fa";
+import Link from 'next/link';
+
+export default function Header() {
+    return (
+        <header className='bg-orange-200 shadow-md'>
+            <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+                <Link href='/'>
+                    <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                        <span className='text-orange-500'>Rent</span>
+                        <span className='text-orange-700'>Easy</span>
+                    </h1>
+                </Link>
+                <form className='bg-orange-100 p-3 rounded-lg flex items-center'>
+                    <input
+                        type='text'
+                        placeholder='Find a Home'
+                        className='bg-transparent focus:outline-none w-24 sm:w-64 text-orange-700'
+                    />
+                    <button>
+                        <FaSearchLocation className='text-orange-600' />
+                    </button>
+                </form>
+                <ul className='flex gap-4'>
+                    <Link href='/'>
+                        <li className='hidden md:inline text-orange-700 hover:underline'>
+                            Home
+                        </li>
+                    </Link>
+                    <Link href='/favourites'>
+                        <li className='hidden md:inline text-orange-700 hover:underline'>
+                            Favourites
+                        </li>
+                    </Link>
+                    <Link href='/about'>
+                        <li className='hidden md:inline text-orange-700 hover:underline'>
+                            About
+                        </li>
+                    </Link>
+                    <Link href='/sign-in'>
+                        <li className='hidden md:inline text-orange-700 hover:underline'>
+                            Sign In
+                        </li>
+                    </Link>
+                </ul>
+            </div>
+        </header>
+    );
+}
