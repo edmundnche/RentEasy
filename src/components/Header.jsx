2d59e1923e9ab3757d2816543bad038ca8b0a9ad
edmundnche <edmundche.nche@edu.sait.ca>
2025-03-01 21:05:35 -0700

    Implement search functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,64 @@
-import { FaSearchLocation } from "react-icons/fa";
+'use client';
+
+import { FaSearch } from 'react-icons/fa';
 import Link from 'next/link';
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 
+import { useEffect, useState } from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
 export default function Header() {
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(searchParams);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl);
+        }
+    }, [searchParams]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(searchParams);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        router.push(`/search?${searchQuery}`);
+    };
+
     return (
-        <header className='bg-[#A6C7FF] shadow-md'>
+        <header className='bg-slate-200 shadow-md'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
                 <Link href='/'>
-                    <h1 className='font-bold text-sm sm:text-xl flex flex-wrap text-[#204FA0]'>
-                        <span>Rent</span>
-                        <span>Easy</span>
+                    <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                        <span className='text-slate-500'>Rent</span>
+                        <span className='text-slate-700'>Easy</span>
                     </h1>
                 </Link>
-                <form className='bg-[#5e5f66] p-3 rounded-lg flex items-center'>
+                <form
+                    className='bg-slate-100 p-3 rounded-lg flex items-center'
+                    onSubmit={handleSubmit}
+                >
                     <input
                         type='text'
-                        placeholder='Find a Home'
-                        className='bg-transparent focus:outline-none w-24 sm:w-64 text-[#282930]'
+                        placeholder='Search...'
+                        className='bg-transparent focus:outline-none w-24 sm:w-64'
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
                     />
                     <button>
-                        <FaSearchLocation className='text-[#204FA0]' />
+                        <FaSearch className='text-slate-600' />
                     </button>
                 </form>
                 <ul className='flex gap-4'>
                     <Link href='/'>
-                        <li className='hidden md:inline text-[#204FA0] hover:underline'>
+                        <li className='hidden md:inline text-slate-700 hover:underline'>
                             Home
                         </li>
                     </Link>
-                    <Link href='/favourites'>
-                        <li className='hidden md:inline text-[#204FA0] hover:underline'>
-                            Favourites
-                        </li>
-                    </Link>
-                    <Link href='/about-page'>
-                        <li className='hidden md:inline text-[#204FA0] hover:underline'>
+                    <Link href='/about'>
+                        <li className='hidden md:inline text-slate-700 hover:underline'>
                             About
                         </li>
                     </Link>
@@ -43,7 +67,7 @@ export default function Header() {
                     </SignedIn>
                     <SignedOut>
                         <Link href='/sign-in'>
-                            <li className='hidden md:inline text-[#204FA0] hover:underline'>
+                            <li className='hidden md:inline text-slate-700 hover:underline'>
                                 Sign In
                             </li>
                         </Link>
